080e12f686c568ebf6a79e5f470a3000a7fa0c87
Dante <dplanterose@gmail.com>
2018-05-29 13:51:26 +0200

    debug

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'dante@coworking.com'
+  default from: 'dante@mosstone.fr'
   layout 'mailer'
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -3,7 +3,7 @@ class Request < ApplicationRecord
   validates :name, presence: true
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, presence: true
   validates :phone_number, presence: true, format: { with: /\A0\d{9}$\z/ }
-  validates :bio, presence: true, length: { minimum: 20 }
+  validates :bio, presence: true, length: { minimum: 7 }
 
   def accept!
     self.update(status: "accepted")
