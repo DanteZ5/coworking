a2e13d5833f613700a83191dce65a09cf703b725
Dante <dplanterose@gmail.com>
2018-05-29 14:59:06 +0200

    routes only for sessions admin use

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
+  devise_for :users, only: :sessions
+
   resources :requests, only: [:index, :new, :create, :update] do
     member do
       get :confirm_email
     end
   end
 
-  devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
